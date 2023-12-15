c6e5b55ee31d795c429390bc236c9a00b9842eae
Aleona Kozhukar <151907396+AlenaKozhukar@users.noreply.github.com>
2023-12-15 11:05:16 +0300

    Загрузка файлов Задачи 1
    void StopSymbolOrNumber

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает 
+// целые числа с консоли. Программа завершается при вводе символа 
+// ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+void StopSymbolOrNumber()
+{
+for(int i = 0; ; i++)
+{
+    Console.WriteLine("Введите число");
+    string stop = Console.ReadLine();
+
+    if(stop == "q")
+    {
+        break;
+    }
+
+int num = Convert.ToInt32(stop);
+int result = num;
+int sum = 0;
+
+while(result > 0)
+{
+sum = sum + result%10;
+result = result/10;
+}
+    if (sum%2 == 0)
+    {
+    break;
+    }
+}
+}
+
+StopSymbolOrNumber();
