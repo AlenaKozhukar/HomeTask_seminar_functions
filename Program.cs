1153773d4d53ea155ad802ae7a6e3b287d1d5b75
Aleona Kozhukar <151907396+AlenaKozhukar@users.noreply.github.com>
2023-12-15 15:30:46 +0300

    Добавлены файлы Задача 3
    Есть ошибка с переворотом массива (вторая часть не переворачивается)

--- Program.cs
+++ Program.cs
@@ -1,34 +1,36 @@
-﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
 
-Console.WriteLine("Введите размер массива");
-int num = Convert.ToInt32(Console.ReadLine());
-
-int[] array = new int[num];
+Console.WriteLine("Введите размер массива"); 
+int num = Convert.ToInt32(Console.ReadLine()); 
+                                            
+int[] array = new int[num]; 
 
 CreateArray();
-System.Console.WriteLine(", количество четных чисел " + FindNumber());
-
-
 int[] CreateArray()
 {
-for(int i = 0; i<array.Length; i++)
+for(int i = 0; i<array.Length; i++) 
 {
-    array[i]= new Random().Next(100,1000); 
+    array[i]= new Random().Next(0,20);  
     Console.Write(array[i]+ " ");
 }
 return array;
 }
 
-int FindNumber()
+Console.WriteLine(); 
+
+ReverseArray();
+
+int[] ReverseArray()
 {
-int countNumber = 0;
-for(int i = 0; i<array.Length; i++)
+int temp;                                
+for (int i = 0; i < array.Length; i++) 
 {
-    if(array[i]%2 ==0)
-    {
-        countNumber = countNumber+1;
-    }
+    temp = array[i];                       
+    array[i] = array[array.Length-i-1];                             
+    array[array.Length-i-1] = temp;
+    Console.Write(array[i] + " ");                 
 }
-return countNumber;
+return array;
 }
+
